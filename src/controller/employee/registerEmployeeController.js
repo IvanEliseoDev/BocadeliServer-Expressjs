c6c6b509a2dd635085e8a46e3aed12aa70615f5d
IvanEliseoDev <20240775@ricaldone.edu.sv>
2026-04-15 12:16:40 -0600

    feat: add register Employee Method

--- src/controller/employee/registerEmployeeController.js
+++ src/controller/employee/registerEmployeeController.js
@@ -1,10 +1,34 @@
+import { EmployeeModel } from "../../models/employee/employeeModel.js";
+import bcryptjs from "bcryptjs";
+import crypto from "crypto";
+import { stringify } from "querystring";
 
-export const RegisterEmployeeController =  []
+export const RegisterEmployeeController = [];
+let codeRandom = "";
 
-RegisterEmployeeController.registerEmployee = async(req, res) =>{
+RegisterEmployeeController.registerEmployee = async (req, res) => {
+  const { name, lastName, email, password, role, salary, phone } = req.body;
+  try {
+    const existEmployee = EmployeeModel.findOne({ email });
+    console.log(existEmployee)
+    if (existEmployee) res.status(400).json({ message: "Employee alredy Exist" });
+    const passwordHashed = await bcryptjs.hash(password, 10);
+    const RandomCode = crypto.randomBytes(3).toString("hex");
+    const InsertedEmployee = EmployeeModel.save({
+      name,
+      lastName,
+      email,
+      password: passwordHashed,
+      role,
+      salary,
+      phone,
+    });
+    codeRandom = RandomCode
+    return res.status(201).json({message: "Employee Inserted Has Succesfuly", data: InsertedEmployee, code: codeRandom})
+  } catch (error) {
+    console.log("Server Error: ", error);
+    throw new Error("Internal Server Error - Check Server Logs ", error);
+  }
+};
 
-}
-
-RegisterEmployeeController.verifiedEmployee = async() => {
-
-}
+RegisterEmployeeController.verifiedEmployee = async () => {};
--- src/routes/employee/employeeRoute.js
+++ src/routes/employee/employeeRoute.js
@@ -1,6 +1,7 @@
 import express from "express"
 import { employeeController } from "../../controller/employee/employeeController.js"
+import { RegisterEmployeeController } from "../../controller/employee/registerEmployeeController.js"
 
 export const EmployeeRouter = express.Router()
 
-EmployeeRouter.route("/").get(employeeController.getEmployees)
+EmployeeRouter.route("/").get(employeeController.getEmployees).post(RegisterEmployeeController.registerEmployee)
